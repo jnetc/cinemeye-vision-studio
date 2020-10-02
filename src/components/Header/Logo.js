1fd5273959b52822c2c13aa5e54a405eb809ac17
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2020-10-02 17:56:52 +0300

    logo path fixed

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
-// Components
-import logo from '../../images/svg/logo.svg';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 const Logo = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(name: { eq: "logo" }) {
+        publicURL
+      }
+    }
+  `);
+  if (!data?.file?.publicURL) {
+    return null;
+  }
+
   return (
     <Link id="logo" to="/">
-      <img src={logo} alt="logo" />
+      <img src={data.file.publicURL} alt="logo" />
     </Link>
   );
 };
