9bb9eccd5d2f6ccec97d674c393361d5e55f6663
Anton <31236341+jnetc@users.noreply.github.com>
2024-03-24 16:25:31 +0200

    Update Icons.js

--- src/components/meetus/buttons/Icons.js
+++ src/components/meetus/buttons/Icons.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Telephone } from './Telephone';
 import { Telegram } from './Telegram';
 import { WhatsApp } from './WhatsApp';
+import { Instagram } from './Instagram';
 
 export const Icons = ({ icon }) => {
   const svg = {
     telephone: <Telephone />,
     telegram: <Telegram />,
+    instagram: <Instagram />,
     whatsapp: <WhatsApp />,
   };
   return svg[icon];
